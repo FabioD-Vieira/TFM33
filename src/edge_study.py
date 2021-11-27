a8f926d03b3fefdf3ba7f41f176cc5a41e4007d8
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2021-11-27 22:19:33 +0000

    Added edge_study.py file

---
+++ src/edge_study.py
@@ -0,0 +1,21 @@
+import cv2
+from matplotlib import pyplot as plt
+
+RED = 2
+GREEN = 1
+BLUE = 0
+
+image_path = "../images/pool.jpeg"
+image = cv2.imread(image_path)
+
+image_red_channel = image[:, :, RED]
+image_green_channel = image[:, :, GREEN]
+image_blue_channel = image[:, :, BLUE]
+
+canny = cv2.Canny(image_red_channel, 100, 200)
+sobel = cv2.Sobel(src=image_red_channel, ddepth=cv2.CV_8U, dx=1, dy=1, ksize=3)
+
+plt.imshow(canny)
+plt.show()
+plt.imshow(sobel)
+plt.show()
