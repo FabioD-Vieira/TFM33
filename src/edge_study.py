ad25c1f36620d7be86d907c8cb5eb86bc69b6459
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2021-12-12 17:25:48 +0000

    Commented code

--- src/edge_study.py
+++ src/edge_study.py
@@ -13,9 +13,12 @@ image_green_channel = image[:, :, GREEN]
 image_blue_channel = image[:, :, BLUE]
 
 canny = cv2.Canny(image_red_channel, 100, 200)
+# canny2 = cv2.Canny(image_red_channel, 200, 255)
 sobel = cv2.Sobel(src=image_red_channel, ddepth=cv2.CV_8U, dx=1, dy=1, ksize=3)
 
 plt.imshow(canny)
 plt.show()
+# plt.imshow(canny2)
+# plt.show()
 plt.imshow(sobel)
 plt.show()
