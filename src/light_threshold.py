ccdd078bee28f8665c5a7f648e5537e7d01201e9
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-03-15 00:25:46 +0000

    Added light_threshold.py

---
+++ src/light_threshold.py
@@ -0,0 +1,20 @@
+import cv2
+
+RED = 2
+GREEN = 1
+BLUE = 0
+
+image_path = "../images/III/9.jpeg"
+image = cv2.imread(image_path)
+image = cv2.resize(image, (0, 0), fx=0.3, fy=0.3)
+
+cv2.imshow("original", image)
+
+image_channel = image[:, :, BLUE]
+cv2.imshow("channel", image_channel)
+
+ret, image_binary = cv2.threshold(image_channel, 245, 255, cv2.THRESH_BINARY)
+cv2.imshow("Binary", image_binary)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
