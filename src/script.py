d00d08c2c9528c65c4517407ef422d69d69b5afc
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-04-20 02:46:00 +0100

    Added test code for lut calculation in script.py. Added script_lut.py

--- src/script.py
+++ src/script.py
@@ -1,6 +1,7 @@
 import glob
 
 import cv2
+import numpy as np
 
 from src.camera import Camera
 from src.system import System
@@ -23,7 +24,7 @@ undistorted = camera.un_distort(img, balance=0.9)
 #         print(x_coord, y_coord)
 #
 #
-# cv2.setMouseCallback("Undistorted", print_coordinates)
+# cv2.setMouseCallback("Original", print_coordinates)
 
 system.calculate_homography_matrix()  # Called only once as well
 reprojected = system.apply_homography(undistorted)
@@ -32,6 +33,47 @@ cv2.imshow("Homography", reprojected)
 
 x, y, angle = system.get_vessel_info(reprojected)
 print(x, y, angle)
+
+gradient = np.zeros([480, 640, 3], dtype=np.float32)
+for i in range(len(gradient)):
+    for j in range(len(gradient[i])):
+        gradient[i][j] = (1, i/479, j/639)
+# cv2.imshow("test", test)
+
+gradient_undistorted = camera.un_distort(gradient, 0.9)
+# cv2.imshow("test2", test2)
+
+gradient_reprojected = system.apply_homography(gradient_undistorted)
+gradient_reprojected[:, :, 1] *= 479
+gradient_reprojected[:, :, 2] *= 639
+
+lut = np.zeros([480, 640, 3], dtype=np.float32)
+lut2 = np.zeros([480, 640, 3], dtype=np.float32)
+for x in range(len(gradient_reprojected)):
+    for y in range(len(gradient_reprojected[x])):
+        pix = np.round(gradient_reprojected[x][y]).astype(int)
+
+        lut[pix[1]][[pix[2]]] = gradient[x][y]
+        lut2[x][y] = pix
+
+kernel = np.ones((3, 3), np.float32)
+lut = cv2.morphologyEx(lut, cv2.MORPH_CLOSE, kernel)
+
+cv2.imshow("lut", lut)
+
+lut2 = lut2.astype(int)
+new_img2 = np.zeros([480, 640, 3], dtype=np.uint8)
+for x in range(len(lut2)):
+    for y in range(len(lut2[x])):
+        _, row, column = lut2[x][y]
+        new_img2[x][y] = img[row][column]
+
+# print(new_img)
+cv2.imshow("lut2 new image", new_img2)
+
+# new_lut = np.array((lut*255), dtype=np.uint8)
+# result = cv2.addWeighted(img, 0.7, new_lut, 0.3, 0.0)
+# cv2.imshow("result", result)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ src/script_lut.py
@@ -0,0 +1,24 @@
+import glob
+
+import cv2
+
+from src.system import System
+
+system = System()
+
+images = glob.glob('../images/calibration/*.jpg')
+system.setup(images, balance=0.9)
+
+print("Finished setup")
+
+img = cv2.imread("../images/pool/img06_fake_leds.jpg")
+new_image = system.process(img)
+
+cv2.imshow("new_image", new_image)
+
+x, y, angle = system.get_vessel_info(new_image)
+print(x, y, angle)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
