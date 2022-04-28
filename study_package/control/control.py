34d63eb1c56efb2040f4454271d8ffb4994f2869
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-04-28 01:22:11 +0100

    Added some logic for PD controller

--- study_package/control/control.py
+++ study_package/control/control.py
@@ -1,3 +1,5 @@
+from time import perf_counter_ns
+
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import sin, cos, pi, linspace
@@ -25,9 +27,48 @@ plt.plot([vessel_x], [vessel_y], marker="o", markersize=2, markeredgecolor="blue
 circle_points = np.stack((arc_xs, arc_ys), axis=1)
 
 vessel_point = (vessel_x, vessel_y)
-dist = np.sqrt(np.sum((circle_points - vessel_point)**2, axis=1))
+distances = np.sqrt(np.sum((circle_points - vessel_point)**2, axis=1))
 
-sorted_indexes = np.argsort(dist)
+min_error_index = np.argmin(distances)
+
+points = np.roll(circle_points, -min_error_index, axis=0)
+errors = np.roll(distances, -min_error_index)
+
+threshold = 1
+
+current_index = 0
+previousError = 0
+previousTime = perf_counter_ns()
+
+KP = 0.1
+KD = 0.1
+
+while True:
+    error = errors[current_index]
+    target = points[current_index]
+
+    target_vector = target - vessel_point
+    print(target_vector)
+
+    timeDelta = perf_counter_ns() - previousTime
+
+    derivative = (error - previousError) / timeDelta
+    previousError = error
+
+    output = KP * error + KD * derivative
+    print(output)
+
+    # vessel updated in the meantime
+
+    error = 0
+    if error < threshold:
+        current_index += 1
+
+    if current_index == len(errors):
+        break
+
+    distances = np.sqrt(np.sum((circle_points - vessel_point) ** 2, axis=1))
+    errors = np.roll(distances, -min_error_index)
+
 
 plt.show()
-
