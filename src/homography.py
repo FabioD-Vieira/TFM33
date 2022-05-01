1dd3c5cbf095c8e9478a2e43fafb6f1d65d60b6c
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-05-01 16:21:58 +0100

    Refactored code in homography.py. Created math_utils.py with intersection point calculation

--- src/homography.py
+++ src/homography.py
@@ -1,29 +1,7 @@
 import cv2
 import numpy as np
 
-
-def get_corner_point(corner, binary_threshold, hough_threshold, min_length, max_gap):
-
-    image_red_channel = corner[:, :, 2]
-
-    _, image_binary = cv2.threshold(image_red_channel, binary_threshold, 255, cv2.THRESH_BINARY)
-    canny = cv2.Canny(image_binary, 100, 200)
-
-    lines = cv2.HoughLinesP(canny, 1, np.pi / 180, hough_threshold, minLineLength=min_length, maxLineGap=max_gap)
-
-    # Find lines intersection point
-    ax, ay, bx, by = lines[0][0]
-    m1 = (by - ay) / (bx - ax)
-    b1 = ay - (m1 * ax)
-
-    cx, cy, dx, dy = lines[1][0]
-    m2 = (dy - cy) / (dx - cx)
-    b2 = cy - (m2 * cx)
-
-    x = (b2 - b1) / (m1 - m2)
-    y = m1 * x + b1
-
-    return np.round(x).astype(int), np.round(y).astype(int)
+from src import math_utils
 
 
 class Homography:
@@ -46,36 +24,49 @@ class Homography:
         self.__bottom_left = 60
         self.__bottom_right = 610
 
+        self.__RED = 2
+
+    def __find_pool_outline(self, corner, binary_threshold, hough_threshold, min_length, max_gap):
+        image_red_channel = corner[:, :, self.__RED]
+
+        _, image_binary = cv2.threshold(image_red_channel, binary_threshold, 255, cv2.THRESH_BINARY)
+        canny = cv2.Canny(image_binary, 100, 200)
+
+        return cv2.HoughLinesP(canny, 1, np.pi / 180, hough_threshold, minLineLength=min_length, maxLineGap=max_gap)
+
+    def __get_corner_section(self, img, y, x):
+        return img[y - self.__size:y + self.__size, x - self.__size:x + self.__size]
+
     def __calculate_source_points(self, image):
 
         # Bottom left
-        ax, ay = get_corner_point(image[self.__bottom-self.__size:self.__bottom+self.__size,
-                                  self.__bottom_left-self.__size:self.__bottom_left+self.__size],
-                                  binary_threshold=5, hough_threshold=30, min_length=40, max_gap=10)
+        corner = self.__get_corner_section(image, self.__bottom, self.__bottom_left)
+        lines = self.__find_pool_outline(corner, binary_threshold=5, hough_threshold=30, min_length=40, max_gap=10)
+        ax, ay = math_utils.calculate_intersection_point(lines)
 
         ay = np.round(ay).astype(int) + self.__bottom - self.__size
         ax = np.round(ax).astype(int) + self.__bottom_left - self.__size
 
         # Top left
-        bx, by = get_corner_point(image[self.__top-self.__size:self.__top+self.__size,
-                                  self.__top_left-self.__size:self.__top_left+self.__size],
-                                  binary_threshold=5, hough_threshold=40, min_length=30, max_gap=10)
+        corner = self.__get_corner_section(image, self.__top, self.__top_left)
+        lines = self.__find_pool_outline(corner, binary_threshold=5, hough_threshold=40, min_length=30, max_gap=10)
+        bx, by = math_utils.calculate_intersection_point(lines)
 
         by = np.round(by).astype(int) + self.__top - self.__size
         bx = np.round(bx).astype(int) + self.__top_left - self.__size
 
         # Top right
-        cx, cy = get_corner_point(image[self.__top-self.__size:self.__top+self.__size,
-                                  self.__top_right-self.__size:self.__top_right+self.__size],
-                                  binary_threshold=30, hough_threshold=30, min_length=30, max_gap=10)
+        corner = self.__get_corner_section(image, self.__top, self.__top_right)
+        lines = self.__find_pool_outline(corner, binary_threshold=30, hough_threshold=30, min_length=30, max_gap=10)
+        cx, cy = math_utils.calculate_intersection_point(lines)
 
         cy = np.round(cy).astype(int) + self.__top - self.__size
         cx = np.round(cx).astype(int) + self.__top_right - self.__size
 
         # Bottom right
-        dx, dy = get_corner_point(image[self.__bottom-self.__size:self.__bottom+self.__size,
-                                  self.__bottom_right-self.__size:self.__bottom_right+self.__size],
-                                  binary_threshold=30, hough_threshold=40, min_length=30, max_gap=10)
+        corner = self.__get_corner_section(image, self.__bottom, self.__bottom_right)
+        lines = self.__find_pool_outline(corner, binary_threshold=30, hough_threshold=40, min_length=30, max_gap=10)
+        dx, dy = math_utils.calculate_intersection_point(lines)
 
         dy = np.round(dy).astype(int) + self.__bottom - self.__size
         dx = np.round(dx).astype(int) + self.__bottom_right - self.__size
