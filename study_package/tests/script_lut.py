bd19ece58ed439463bcd8cdf404de82240c7c510
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-05-01 11:23:40 +0100

    Added scripts tests

--- study_package/tests/script_lut.py
+++ study_package/tests/script_lut.py
@@ -4,14 +4,14 @@ import cv2
 
 from src.system import System
 
-system = System()
+system = System(camera_resolution=(640, 480), camera_balance=0.9)
 
 images = glob.glob('../images/calibration/*.jpg')
-system.setup(images, balance=0.9)
+system.setup(images)
 
 print("Finished setup")
 
-img = cv2.imread("../images/pool/img06_fake_leds.jpg")
+img = cv2.imread("../../images/pool/img06_fake_leds.jpg")
 new_image = system.process(img)
 
 cv2.imshow("new_image", new_image)
---
+++ study_package/tests/script_old_image.py
@@ -0,0 +1,37 @@
+import glob
+
+import cv2
+
+from src.camera import Camera
+from src.system import System
+
+system = System()
+camera = Camera()
+
+images = glob.glob('../images/calibration/*.jpg')
+camera.calibrate(images)  # Called only once to calibrate
+
+img = cv2.imread("../../images/pool/img06_fake_leds.jpg")
+cv2.imshow("Original", img)
+
+undistorted = camera.un_distort(img, balance=0.9)
+# cv2.imshow("Undistorted", undistorted)
+
+
+def print_coordinates(event, x_coord, y_coord, _, _1):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        print(x_coord, y_coord)
+
+
+cv2.setMouseCallback("Original", print_coordinates)
+
+system.calculate_homography_matrix()  # Called only once as well
+reprojected = system.apply_homography(undistorted)
+
+cv2.imshow("Homography", reprojected)
+
+# x, y, angle = system.get_vessel_info(reprojected)
+# print(x, y, angle)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
