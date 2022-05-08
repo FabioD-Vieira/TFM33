4b3495aa4b0d841d633a7ec40e4f7755584fd67a
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-05-08 23:29:39 +0100

    Added circle comments

--- src/shape/circle.py
+++ src/shape/circle.py
@@ -20,11 +20,15 @@ class Circle(Shape):
 
     def __create_circle(self, number_of_checkpoints, radius):
         center_x, center_y = self.__center
+
+        # create X points from 0 to 2Pi
         arc_angles = np.linspace(0 * np.pi, 2 * np.pi, number_of_checkpoints)
 
+        # Add correct positions to each point
         arc_xs = (radius * np.cos(arc_angles)) + center_x
-
         arc_ys = (radius * np.sin(arc_angles)) + center_y
+
+        # Order counter clock wise
         arc_ys = arc_ys[::-1]
 
         return np.stack((arc_ys, arc_xs), axis=1)
@@ -42,14 +46,18 @@ class Circle(Shape):
 
     def calculate_points_orientations(self):
 
+        # Matrix to rotate 90 degrees
         theta = np.deg2rad(90)
         rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
+        # Calculate vectors to the center of the circle
         vectors_to_center = self._points - [self.__center[1], self.__center[0]]
 
+        # Calculate tangent vector of each circle point by rotating 90º each vector to the center
         self._points_orientations = np.zeros(len(self._points))
         for i in range(len(vectors_to_center)):
             vector = np.dot(rot, vectors_to_center[i])
             self._points_orientations[i] = math.degrees(math.atan2(vector[0], vector[1]))
 
+        # Roll the array to be consistent with sorted points
         self._points_orientations = np.roll(self._points_orientations, -self._min_error_index, axis=0)
