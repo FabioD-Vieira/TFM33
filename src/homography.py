3a60d40fb33ac57510638f44842e87a6c225e627
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-05-08 23:55:52 +0100

    Added homography comments

--- src/homography.py
+++ src/homography.py
@@ -8,6 +8,7 @@ class Homography:
         self.__width, self.__height = camera_resolution
         self.__h = None
 
+        # base image pool corners
         self.__base_corners = [(199, 192), (195, 454), (324, 37), (320, 638)]
 
         self.__sift = cv2.SIFT_create(contrastThreshold=0.01, edgeThreshold=10)
@@ -17,6 +18,7 @@ class Homography:
 
     def __find_difference(self, corner_base_img, corner_img):
 
+        # detect features in each image and compare them
         kp1, des1 = self.__sift.detectAndCompute(corner_base_img[:, :, 2], None)
         kp2, des2 = self.__sift.detectAndCompute(corner_img[:, :, 2], None)
 
@@ -26,26 +28,32 @@ class Homography:
         vector = np.array([0, 0], dtype='float')
         number_of_features = 0
         for m, n in matches:
-            # Apply ratio test
+            # Apply ratio test to remove useless features
             if m.distance < ratio_dist * n.distance:
                 (x1, y1) = kp1[m.queryIdx].pt
                 (x2, y2) = kp2[m.trainIdx].pt
 
+                # calculate difference between each feature match and sum it in a single vector
                 vector += np.array([x2 - x1, y2 - y1])
                 number_of_features += 1
 
+        # Represent matches in a single image
         # good = [[m] for m, n in matches if m.distance < ratio_dist * n.distance]
         # img3 = cv2.drawMatchesKnn(corner_base_img, kp1, corner_img, kp2, good, None,
         #                           flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
         #
         # cv2.imshow("match", img3)
 
+        # vector mean
         vector = vector / number_of_features
         vector_x, vector_y = np.round(vector).astype(int)
 
         return vector_y, vector_x
 
     def __calculate_source_points(self, base_image, image):
+
+        # Calculates the difference between two images for each corner section
+        # and adds the difference to each know corner point
 
         # Top left
         vector_y, vector_x = self.__find_difference(base_image[150:250, 150:250], image[150:250, 150:250])
