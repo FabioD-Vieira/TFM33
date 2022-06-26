b56323dd4cbd7f437ae8be133837c1279ba5c0e0
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-06-26 22:19:58 +0100

    Commented code in control_clean.py

--- src/vessel_control/control_clean.py
+++ src/vessel_control/control_clean.py
@@ -1,7 +1,7 @@
 import math
 
 import numpy as np
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 from sklearn.neighbors import NearestNeighbors
 
@@ -13,60 +13,14 @@ AC_max_power = 20
 
 
 # To plot line/circle/vessel
-plt.xlim(0, pool_dim[0])
-plt.ylim(pool_dim[1], 0)
-plt.gca().set_aspect('equal')
+# plt.xlim(0, pool_dim[0])
+# plt.ylim(pool_dim[1], 0)
+# plt.gca().set_aspect('equal')
 
 
 # Variables to create line/circle
 number_of_points = 1000
 
-# radius = 3
-
-# Matrix to rotate 90 degrees
-# theta = np.deg2rad(90)
-# rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
-
-
-# Create concave curve
-# arc_angles = np.linspace(np.pi, 1.5 * np.pi, number_of_points)
-#
-# arc_center = (11, 6)
-# arc_xs = (radius * np.cos(arc_angles)) + arc_center[0]
-# arc_ys = (radius * np.sin(arc_angles)) + arc_center[1]
-#
-# # plt.plot(arc_xs, arc_ys, color='red', lw=3)
-#
-# concave_arc = np.stack((arc_xs, arc_ys), axis=1)
-#
-# # Calculate vectors to the center of the circle
-# vectors_to_center = concave_arc - [arc_center[0], arc_center[1]]
-#
-# # Calculate tangent vector of each circle point by rotating 90º each vector to the center
-# concave_points_orientations = np.zeros(len(concave_arc))
-# for i in range(len(vectors_to_center)):
-#     vector = np.dot(rot, vectors_to_center[i])
-#     concave_points_orientations[i] = math.degrees(math.atan2(vector[1], vector[0]))
-
-# Create convex curve
-# arc_angles = np.linspace(0.5 * np.pi, 0 * np.pi, number_of_points)
-#
-# arc_center = (7, 2)
-# arc_xs = (radius * np.cos(arc_angles)) + arc_center[0]
-# arc_ys = (radius * np.sin(arc_angles)) + arc_center[1]
-#
-# # plt.plot(arc_xs, arc_ys, color='red', lw=3)
-#
-# convex_arc = np.stack((arc_xs, arc_ys), axis=1)
-#
-# # Calculate vectors to the center of the circle
-# vectors_to_center = convex_arc - [arc_center[0], arc_center[1]]
-#
-# # Calculate tangent vector of each circle point by rotating 90º each vector to the center
-# convex_points_orientations = np.zeros(len(convex_arc))
-# for i in range(len(vectors_to_center)):
-#     vector = np.dot(rot, vectors_to_center[i])
-#     convex_points_orientations[i] = math.degrees(math.atan2(vector[1], vector[0])) - 180
 
 # Create line
 target_angle = 0
@@ -80,7 +34,7 @@ line_end_y = line_start_y + math.sin(math.radians(target_angle)) * 10
 line_x = np.linspace(line_start_x, line_end_x, number_of_points)
 line_y = np.linspace(line_start_y, line_end_y, number_of_points)
 
-plt.plot(line_x, line_y, color='red', lw=3)
+# plt.plot(line_x, line_y, color='red', lw=3)
 
 line = np.stack((line_x, line_y), axis=1)
 
@@ -89,7 +43,7 @@ line = np.stack((line_x, line_y), axis=1)
 x, y = 3, 5
 angle = 0
 
-plt.plot([x], [y], marker="o", markersize=2, markeredgecolor="blue")
+# plt.plot([x], [y], marker="o", markersize=2, markeredgecolor="blue")
 
 
 # Find the closest point
@@ -113,12 +67,6 @@ while True:
 
     min_error_index = knn.kneighbors([(x, y)], return_distance=False)[0][0]
     target_point = line[min_error_index]
-
-    # target_point = concave_arc[min_error_index]
-    # target_angle = concave_points_orientations[min_error_index]
-
-    # target_point = convex_arc[min_error_index]
-    # target_angle = convex_points_orientations[min_error_index]
 
     if x < 2 or x > 10 or y < 2 or y > 8:
         AV = 0
@@ -159,4 +107,4 @@ while True:
 
     break
 
-plt.show()
+# plt.show()
