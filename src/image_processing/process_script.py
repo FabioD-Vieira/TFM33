63166148f247898d31822670929098f3665827b7
Fábio D. Vieira <72528657+FabioD-Vieira@users.noreply.github.com>
2022-09-11 18:08:46 +0100

    Improved process

--- src/image_processing/process_script.py
+++ src/image_processing/process_script.py
@@ -8,17 +8,19 @@ lut = np.load("lut.npy")
 
 process = Process(lut, pool_dim)
 
-cap = cv2.VideoCapture("../../images/videos/vid05.h264")
+cap = cv2.VideoCapture("../../images/videos/vid07.h264")
 
 while True:
     ret, frame = cap.read()
+    cv2.imshow("f", frame)
 
     if not ret:
         break
 
     process.start(frame)
+    cv2.waitKey(1)
 
 cap.release()
 
-cv2.waitKey(0)
+
 cv2.destroyAllWindows()
